9bd75731467f1e12a1d0dfa8573ff970a826eaa1
Lucas Mikelvin <lucas.mikelvin.matos@accenture.com>
2021-12-05 01:11:20 -0300

    Download via endpoint finalizado

--- src/main/java/br/com/bandtec/tellusspringboot/controllers/PagamentoController.java
+++ src/main/java/br/com/bandtec/tellusspringboot/controllers/PagamentoController.java
@@ -1,6 +1,7 @@
 package br.com.bandtec.tellusspringboot.controllers;
 
 import br.com.bandtec.tellusspringboot.domains.*;
+import br.com.bandtec.tellusspringboot.handlers.PagamentoHandler;
 import br.com.bandtec.tellusspringboot.repositories.*;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -12,15 +13,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+import java.util.UUID;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.Font;
@@ -84,91 +86,50 @@ public class PagamentoController {
 
     }
 
-    @GetMapping("/boleto")
-    public ResponseEntity getBoleto(@RequestBody Pagamento pagamento, @RequestBody String cnpj, @RequestBody String cpf) {
-        Responsavel responsavel = responsavelRepository.findResponsavelByCpf(cpf);
-        Escola escola = escolaRepository.findByCnpj(cnpj);
+    @GetMapping("/download/boleto")
+    @ResponseBody
+    public ResponseEntity getBoleto(
+            @RequestBody Pagamento pagamento, @RequestBody String cnpj, @RequestBody String cpf)
+            throws FileNotFoundException {
+        String nameBoleto = String.format("Boleto-462cd0f0-557e-11ec-bf63-0242ac130002.pdf");
+        PagamentoHandler pagamentoHandler = new PagamentoHandler();
+        pagamentoHandler.gerarBoleto(cpf, cnpj, pagamento, nameBoleto);
 
-        Document doc = new Document();
-        try {
-            PdfWriter.getInstance(doc, new FileOutputStream("boleto.pdf"));
-            doc.open();
+        File file = new File(nameBoleto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Content-Type", "application/pdf");
+        headers.add("Content-Disposition", String.format("attachment; filename=%s", nameBoleto));
 
-            // Pripriedade de table, para modelar boleto
-            PdfPTable linha1 = new PdfPTable(1);
-            PdfPTable linha2 = new PdfPTable(2);
-            PdfPTable linha3 = new PdfPTable(2);
-            PdfPTable linha4 = new PdfPTable(4);
-            PdfPTable linha5 = new PdfPTable(3);
-            PdfPTable linha6 = new PdfPTable(2);
-            PdfPTable linha7 = new PdfPTable(1);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
 
-            Font fontBody = new Font(Font.FontFamily.HELVETICA, 10.0f, Font.UNDEFINED, BaseColor.BLACK);
-            Font fontBodyBold = new Font(Font.FontFamily.HELVETICA, 10.0f, Font.BOLD, BaseColor.BLACK);
+    @GetMapping("/teste-download")
+    @ResponseBody
+    public ResponseEntity getBoletoMockado() throws FileNotFoundException {
+        PagamentoHandler pagamentoHandler = new PagamentoHandler();
+        String nameBoleto = String.format("Boleto-462cd0f0-557e-11ec-bf63-0242ac130002.pdf");
+        pagamentoHandler.gerarBoletoMockado(nameBoleto);
+        File file = new File(nameBoleto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Content-Type", "application/pdf");
+        headers.add("Content-Disposition", String.format("attachment; filename=%s", nameBoleto));
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
 
-            // Celulas do boleto
-            PdfPCell cellCode = new PdfPCell(new Paragraph(String.format("%s", "36490.00019 00014.978100 00000.490482 7 88000000010990"), fontBodyBold));
-            PdfPCell cellLocalPagamento = new PdfPCell(new Paragraph(String.format("Local do Pagamento\n%s", "Qualquer banco até a data de vencimento."), fontBody));
-            PdfPCell cellDataVencimento = new PdfPCell(new Paragraph(String.format("Vencimento\n%s", pagamento.getDataVenc()), fontBody));
-            PdfPCell cellBeneficiario = new PdfPCell(new Paragraph(String.format("Beneficiário\n%s", escola.getRazaoSocial()), fontBody));
-            PdfPCell cellAgencia = new PdfPCell(new Paragraph(String.format("Agência / Código do Beneficiário\n%s", "4218/1234567"), fontBody));
-            PdfPCell cellNumDoc = new PdfPCell(new Paragraph(String.format("Número Documento\n%s", "54646"), fontBody));
-            PdfPCell cellEspecieDoc = new PdfPCell(new Paragraph(String.format("Espécie Doc.\n%s", "ND"), fontBody));
-            PdfPCell cellAceite = new PdfPCell(new Paragraph(String.format("Aceite\n%s", "S"), fontBody));
-            PdfPCell cellNumDocumento = new PdfPCell(new Paragraph(String.format("Nosso Número\n%s", "0000089-2"), fontBody));
-            PdfPCell cellCarteira = new PdfPCell(new Paragraph(String.format("Carteira\n%s", "102"), fontBody));
-            PdfPCell cellEspecie = new PdfPCell(new Paragraph(String.format("Espécie\n%s", "R$"), fontBody));
-            PdfPCell cellValor = new PdfPCell(new Paragraph(String.format("(=) Valor do Documento\n%s", pagamento.getValor()), fontBody));
-            PdfPCell cellName = new PdfPCell(new Paragraph(String.format("Pagador\n%s", responsavel.getNome()), fontBody));
-            PdfPCell cellCpf = new PdfPCell(new Paragraph(String.format("CPF\n%s", responsavel.getCpf()), fontBody));
-            Image img = Image.getInstance("codebar.png");
-            img.scaleAbsolute(350f, 40f);
-
-            PdfPCell cellImage = new PdfPCell(img);
-            cellImage.setPadding(1);
-            cellImage.setBorderColorBottom(new BaseColor(255, 255, 255, 0));
-            cellImage.setBorderColorLeft(new BaseColor(255, 255, 255, 0));
-            cellImage.setBorderColorRight(new BaseColor(255, 255, 255, 0));
-
-            // Adicionando celular a tabela
-            linha1.addCell(cellCode);
-            linha2.addCell(cellLocalPagamento);
-            linha2.addCell(cellDataVencimento);
-            linha3.addCell(cellBeneficiario);
-            linha3.addCell(cellAgencia);
-            linha4.addCell(cellNumDoc);
-            linha4.addCell(cellEspecieDoc);
-            linha4.addCell(cellAceite);
-            linha4.addCell(cellNumDocumento);
-            linha5.addCell(cellCarteira);
-            linha5.addCell(cellEspecie);
-            linha5.addCell(cellValor);
-            linha6.addCell(cellName);
-            linha6.addCell(cellCpf);
-            linha7.addCell(cellImage);
-
-            // Adicionando tabelas ao arquivo
-            doc.add(linha1);
-            doc.add(linha2);
-            doc.add(linha3);
-            doc.add(linha4);
-            doc.add(linha5);
-            doc.add(linha6);
-            doc.add(linha7);
-
-            // Fechando documento
-            doc.close();
-
-            // Abre documento
-            Desktop.getDesktop().open(new File("boleto.pdf"));
-            return ResponseEntity.status(200).body("test");
-
-        } catch (DocumentException de) {
-            return ResponseEntity.status(400).body(de.getMessage());
-        } catch (FileNotFoundException e) {
-            return ResponseEntity.status(400).body(e.getMessage());
-        } catch (IOException e) {
-            return ResponseEntity.status(400).body(e.getMessage());
-        }
     }
 }
